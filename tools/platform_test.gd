f3706deaad198441b5682aa3f00ddbe7cbde295c
GalaxyMom <galaxymomtwitch@gmail.com>
2025-06-03 11:54:11 -0700

    (update) direction setter

--- tools/platform_test.gd
+++ tools/platform_test.gd
@@ -6,20 +6,23 @@ const STATES = Globals.STATES
 @export_tool_button("Generate Jump Path") var jump_path_button = jump_path_create
 @export_tool_button("Clear Lines") var clear_lines_button = clear_lines
 @export_tool_button("Reset Position") var reset_position_button = reset_position
+@export_enum("Left:-1", "Right:1") var test_direction : int = -1
 
 @onready var position_timer: Timer = %PositionTimer
 
+var _default_position := Vector2.ZERO
 var _draw_points : Array
 
 func jump_path_create() -> void:
+	_default_position = global_position
 	_draw_points = [global_position]
 	get_parent().change_state.emit(STATES.JUMPING)
-	get_parent().direction = 1.0
+	get_parent().direction = test_direction
 	position_timer.start()
 
 
 func reset_position() -> void:
-	get_parent().global_position = Vector2.ZERO
+	get_parent().global_position = _default_position
 
 
 func clear_lines() -> void:
@@ -27,18 +30,18 @@ func clear_lines() -> void:
 	for i in range(temp_nodes.size()):
 		temp_nodes[i].queue_free()
 
+
 func _on_position_timer_timeout() -> void:
+	_draw_points.append(global_position)
 	if get_parent().is_on_floor():
 		get_parent().direction = 0.0
 		draw_trajectory()
 		return
 	
-	_draw_points.append(global_position)
 	position_timer.start()
 
 
 func draw_trajectory() -> void:
-	print("drawing")
 	var new_line := Line2D.new()
 	new_line.add_to_group("Temp")
 	for i in range(_draw_points.size()):
