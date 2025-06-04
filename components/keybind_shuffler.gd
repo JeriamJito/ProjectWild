52b100fb8b984b244e2ad644959e89087c2afd50
GalaxyMom <galaxymomtwitch@gmail.com>
2025-06-04 12:42:05 -0700

    (feat) keybind shuffler

---
+++ components/keybind_shuffler.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+@export var keyswap : Keyswap
+
+var compiled_actions : Dictionary[String, InputEvent]
+
+func _ready() -> void:
+	compile_keybinds()
+
+func compile_keybinds() -> void:
+	compiled_actions = {
+		"move_left": keyswap.left,
+		"move_right": keyswap.right,
+		"jump": keyswap.jump,
+		"drop_down": keyswap.drop,
+		"whip_use": keyswap.whip,
+	}
+
+func _on_body_entered(body: Node2D) -> void:
+	if get_tree().get_first_node_in_group("Player") != body:
+		return
+
+	for key in compiled_actions:
+		InputMap.action_erase_events(key)
+		InputMap.action_add_event(key, compiled_actions[key])
