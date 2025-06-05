900f339e29ad0296d6df3822256766b1979b8690
GalaxyMom <galaxymomtwitch@gmail.com>
2025-06-04 21:41:50 -0700

    (fix) missing animation error

--- player/player_sprite.gd
+++ player/player_sprite.gd
@@ -5,7 +5,9 @@ const STATES = Globals.STATES
 @onready var parent : Actor = get_parent()
 
 func _physics_process(_delta: float) -> void:
-	play(STATES.keys()[parent.state])
+	var animation_name : String = STATES.keys()[parent.state]
+	if sprite_frames.has_animation(animation_name):
+		play(animation_name)
 		
 	if flip_h and parent.direction < 0.0 or not flip_h and parent.direction > 0.0:
 		flip_h = !flip_h
