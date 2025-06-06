35c0e197175585dbe642f46b7e8a9fa2101d2cef
GalaxyMom <galaxymomtwitch@gmail.com>
2025-06-05 19:08:44 -0700

    (update) quit gameplay with ESC

--- stage.gd
+++ stage.gd
@@ -2,3 +2,8 @@ extends Node2D
 class_name World
 
 var state := Globals.WORLD_STATES.RUNNING
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("ui_cancel"):
+		MenuMusic.play()
+		get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
